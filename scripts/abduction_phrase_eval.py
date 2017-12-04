7d591b8b5a53d760cadacfc241e5443f4c20a031
谷中瞳 <yanakahitomi@taninakahitominoMacBook-Pro.local>
2017-12-04 21:59:47 +0900

    excluded copy function

--- scripts/abduction_phrase_eval.py
+++ scripts/abduction_phrase_eval.py
@@ -159,10 +159,10 @@ def get_phrases(premise_preds, conclusion_pred, pred_args, expected):
     conclusion_pred = conclusion_pred.split()[0]
     trg_pred = denormalize_token(conclusion_pred)
     for src_pred in src_preds:
-        if src_pred == trg_pred:
+        #if src_pred == trg_pred:
             #the same premise can be found(copy)
-            copyflg = 1
-            break
+            #copyflg = 1
+            #break
         nor_src_pred = normalize_token(src_pred)
         allarg_list = list(set(pred_args[conclusion_pred] + pred_args[nor_src_pred]))
         allarg_list = check_case_from_list(allarg_list)
@@ -171,10 +171,10 @@ def get_phrases(premise_preds, conclusion_pred, pred_args, expected):
         #to do: select best axiom from premises(or make phrasal axiom coq_script)
         if axiom:
             axioms.append(axiom)
-    if copyflg == 1:
-        axiom = 'Axiom ax_copy_{0} : forall x, _{0} x.'\
-        .format(trg_pred)
-        axioms.append(axiom)
+    #if copyflg == 1:
+    #    axiom = 'Axiom ax_copy_{0} : forall x, _{0} x.'\
+    #    .format(trg_pred)
+    #    axioms.append(axiom)
     print("premise_pred:{0}, conclusion_pred:{1}, pred_args:{2}, axiom:{3}".format(premise_preds, conclusion_pred, pred_args, axioms), file=sys.stderr)
     # to do: consider how to inject antonym axioms
 
